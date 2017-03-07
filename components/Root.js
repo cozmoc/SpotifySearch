af0ae5e1fb528cfe2fcf151d2b27b9ada16a6867
cozmoc <cozmocozi@gmail.com>
2017-03-07 11:35:43 +0200

    Using ReactNative

---
+++ components/Root.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Navigator } from 'react-native';
+import Main from './Main';
+import Artist from './Artist';
+import NavigationBar from './NavigationBar';
+
+export default class Root extends Component {
+  renderScene(route, navigator) {
+    if (route.id === 'MAIN') {
+      return <Main navigator={ navigator } />;
+    }
+
+    return <Artist url={ route.url } />;
+  }
+
+  render() {
+    return (
+      <Navigator style={{ flex: 1 }}
+        initialRoute={{ id: 'MAIN', title: 'Spotify Artist Search' }}
+        renderScene={ this.renderScene }
+        navigationBar={ NavigationBar }
+      />
+    );
+  }
+}
---
+++ utils/clrs.js
@@ -0,0 +1,10 @@
+export default {
+  black: '#444',
+  white: '#fff',
+  blue: '#3456DA',
+  red: '#F91515',
+  gray: '#aaa',
+  pink: '#efbdeb',
+  mauve: '#b68cb8',
+  green: '#008000'
+};
